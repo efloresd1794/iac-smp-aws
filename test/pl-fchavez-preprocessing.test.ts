da76dce968370f90f86ac59ad13c103318a04197
Edson Flores <efloresd1794@gmail.com>
2025-02-03 11:47:46 -0500

    Initial commit

---
+++ test/pl-fchavez-preprocessing.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PlFchavezPreprocessing from '../lib/pl-fchavez-preprocessing-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/pl-fchavez-preprocessing-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PlFchavezPreprocessing.PlFchavezPreprocessingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
